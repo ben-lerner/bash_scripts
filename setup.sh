0ad6775ec66c7bd433a269b12b3d5259636b290e
Ben Lerner <bl@uber.com>
2016-08-10 16:14:12 -0400

    add scheme to setup.sh

--- setup.sh
+++ setup.sh
@@ -3,3 +3,10 @@ brew install coreutis tree ag
 # emacs
 ln -s /Applications/Emacs.app/Contents/MacOS/bin/emacsclient /usr/local/bin
 # follow: https://emacsformacosx.com/tips
+
+# scheme:
+# https://jacksonisaac.wordpress.com/2014/03/25/installing-scheme-on-mac-os-x/
+
+sudo ln -s /Applications/MIT\:GNU\ Scheme.app/Contents/Resources /usr/local/lib/mit-scheme-x86-64
+sudo ln -s /usr/local/lib/mit-scheme-x86-64/mit-scheme /usr/local/bin/scheme
+
