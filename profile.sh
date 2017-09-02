a5780536dc1501be829b8e8ae7ac020089a22326
Ben Lerner <bl@nlglgn.com>
2017-09-02 19:12:24 -0400

    change timer to hours, minutes, seconds

--- profile.sh
+++ profile.sh
@@ -125,6 +125,24 @@ function timer_start {
 
 function timer_stop {
     timer_show=$(($SECONDS - $timer))
+    if [ "$timer_show" -eq 0 ]; then
+        timer_show=""
+    elif [ "$timer_show" -lt "60" ]; then
+        timer_show=$timer_show$"s"
+    elif [ "$timer_show" -lt "570" ]; then
+        timer_show=$(($timer_show / 60))"m "$(($timer_show % 60))"s"
+    elif [ "$timer_show" -lt "3570" ]; then
+        timer_show=$(($timer_show + 30)) # round
+        timer_show=$(($timer_show / 60))"m"
+    else
+        timer_show=$(($timer_show + 30)) # round
+        timer_show=$(($timer_show / 3600))"h "$(( ($timer_show % 3600) / 60 ))"m"
+    fi
+
+    if [ "$timer_show" != "" ]; then
+        timer_show=$timer_show" :: "
+    fi
+
     unset timer
 }
 
@@ -135,4 +153,4 @@ source "$DIR/git-prompt.sh"
 
 export GIT_PS1_SHOWCOLORHINTS="true"
 export GIT_PS1_SHOWUPSTREAM="auto verbose"# verbose
-export PROMPT_COMMAND='timer_stop; __git_ps1 "${timer_show}s :: \W" " [λ] "; timer_stop'
+export PROMPT_COMMAND='timer_stop; __git_ps1 "${timer_show}\W" " [λ] "; timer_stop'
