1550b482c13db87050e9823ac2b9f1eb136e6b03
Ben Lerner <bl@nlglgn.com>
2018-02-19 23:22:55 -0500

    add profiles.clj

---
+++ profiles.clj
@@ -0,0 +1,7 @@
+{:user {:plugins [[lein-try "0.4.3"]
+                  [lein-exec "0.3.1"]
+;                  [refactor-nrepl "2.3.1"]
+;                  [cider/cider-nrepl "0.17.0-snapshot"]
+                  ]}
+ :repl {:dependencies [[org.clojure/clojure "1.9.0"]
+                       [^:display org.clojure/clojure "1.9.0"]]}}
